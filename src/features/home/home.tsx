8b4b5c3defe6a1559cb52806437ba02f8bfa9be5
Mihai Smolenschi <mihai.smolenschi@sevio.com>
2023-07-05 17:07:45 +0300

    Added carousel

--- src/features/home/home.tsx
+++ src/features/home/home.tsx
@@ -1,18 +1,21 @@
 import {Carousel, Contact} from "../../components";
 import "./home.scss";
+import img1 from "../../gallery/img1.jpeg";
+import img2 from "../../gallery/img2.jpeg";
+import bg from "../../gallery/background.jpeg";
 
 export const Home = () => {
-  return (
-    <div className="home">
-        <Carousel>
-            <img alt="img1" src="images/gallery/img1.jpeg" />
-            <img alt="img2" src="images/gallery/img2.jpeg" />
-            <img alt="img3" src="images/gallery/img1.jpeg" />
-            <img alt="img4" src="images/gallery/img2.jpeg" />
-        </Carousel>
-      <Contact />
-    </div>
-  );
+    const images = [img1, img2, img1, img2];
+
+    return (
+        <div className="home">
+            <div className="home-background" style={{backgroundImage: `url(${bg})`}}></div>
+            <Carousel>
+                {images.map((img, index) => <img alt={`img-${index}`} src={img} key={`img-${index}`}/>)}
+            </Carousel>
+            <Contact/>
+        </div>
+    );
 };
 
 export default Home;
