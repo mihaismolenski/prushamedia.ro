32615b0dd0861f01515ba6553f236d5a39db36ac
Mihai Smolenschi <mihai.smolenschi@sevio.com>
2023-07-06 14:26:20 +0300

    Added portofolio images cards

--- src/features/portofolio/portofolio.tsx
+++ src/features/portofolio/portofolio.tsx
@@ -1,6 +1,81 @@
 import "./portofolio.scss";
+import story1Img from "../../gallery/stories/story1.jpeg";
+import story2Img from "../../gallery/stories/story2.jpeg";
+import story3Img from "../../gallery/stories/story3.jpeg";
+import story4Img from "../../gallery/stories/story4.jpeg";
 
 export const Portofolio = () => {
-    return <div className="portofolio">Portofolio</div>
-}
+  return (
+    <div className="portofolio">
+      <div className="portofolio-content">
+        <div className="column">
+          <div>
+            <img alt="portofolio-image" src={story1Img} />
+          </div>
+          <div>
+            <img alt="portofolio-image" src={story2Img} />
+          </div>
+          <div>
+            <img alt="portofolio-image" src={story3Img} />
+          </div>
+        </div>
+        <div className="column">
+          <div>
+            <img alt="portofolio-image" src={story2Img} />
+          </div>
+          <div>
+            <img alt="portofolio-image" src={story3Img} />
+          </div>
+          <div>
+            <img alt="portofolio-image" src={story4Img} />
+          </div>
+        </div>
+        <div className="column">
+          <div>
+            <img alt="portofolio-image" src={story3Img} />
+          </div>
+          <div>
+            <img alt="portofolio-image" src={story4Img} />
+          </div>
+          <div>
+            <img alt="portofolio-image" src={story1Img} />
+          </div>
+        </div>
+        <div className="column">
+          <div>
+            <img alt="portofolio-image" src={story1Img} />
+          </div>
+          <div>
+            <img alt="portofolio-image" src={story3Img} />
+          </div>
+          <div>
+            <img alt="portofolio-image" src={story4Img} />
+          </div>
+        </div>
+        <div className="column">
+          <div>
+            <img alt="portofolio-image" src={story1Img} />
+          </div>
+          <div>
+            <img alt="portofolio-image" src={story2Img} />
+          </div>
+          <div>
+            <img alt="portofolio-image" src={story3Img} />
+          </div>
+        </div>
+        <div className="column">
+          <div>
+            <img alt="portofolio-image" src={story1Img} />
+          </div>
+          <div>
+            <img alt="portofolio-image" src={story2Img} />
+          </div>
+          <div>
+            <img alt="portofolio-image" src={story3Img} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Portofolio;
